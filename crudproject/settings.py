1f7673cb32b55942308833bb4ba8761db4c0740e
shiva <shiva8184951@gmail.com>
2025-09-30 12:57:26 +0530

    crud project

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import os
 import dj_database_url
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR=BASE_DIR/"templates"
@@ -16,7 +17,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".onrender.com"]
+ALLOWED_HOSTS = ["localhost",".onrender.com"]
 
 
 # Application definition
@@ -66,9 +67,17 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
